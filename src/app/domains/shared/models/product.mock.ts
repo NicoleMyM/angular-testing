1644aeaf86057dee09e521ebad885b7d6d438aa5
NicoleMyM <nicolematurana@rocketmail.com>
2025-05-15 12:09:07 -0400

    Added Cap 12 to Cap 22

--- src/app/domains/shared/models/product.mock.ts
+++ src/app/domains/shared/models/product.mock.ts
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker';
 import { Product } from './product.model';
+import { generateFakeCategory } from './category.mock';
 
 export const generateFakeProduct = (data?: Partial<Product>): Product => ({
   id: faker.number.int(),
@@ -7,12 +8,7 @@ export const generateFakeProduct = (data?: Partial<Product>): Product => ({
   price: parseFloat(faker.commerce.price()),
   description: faker.commerce.productDescription(),
   images: [faker.image.url(), faker.image.url()],
-  category: {
-    id: faker.number.int(),
-    name: faker.commerce.department(),
-    image: faker.image.url(),
-    slug: faker.lorem.slug(),
-  },
+  category: generateFakeCategory(),
   creationAt: faker.date.past().toISOString(),
   slug: faker.lorem.slug(),
   ...data,
